821da34615d23d77e515108347f33e7d7e813db1
Lavilley <lavilleyvincent@gmail.com>
2023-05-03 15:06:10 +0200

    Quelques fichiers JSON et la partie envoi de l'API

---
+++ data.py
@@ -0,0 +1,29 @@
+import os
+
+class PN :
+    
+    # set root_path to : '/home/vincent/projetPN89'
+    root_path = os.path.dirname(__file__)
+
+        
+    def get_barrier_data(self):
+        barrier_path = os.path.join(self.root_path,"data","barrier_files")
+        barrier_files = [f for f in os.listdir(barrier_path) if f.endswith(".json")]
+        return barrier_files
+        
+    def get_bell_data(self):
+        bell_path = os.path.join(self.root_path,"data","bell_files")
+        bell_files = [f for f in os.listdir(bell_path) if f.endswith(".json")]
+        return bell_files
+    
+    def get_light_data(self):
+        light_path = os.path.join(self.root_path,"data","light_files")
+        light_files = [f for f in os.listdir(light_path) if f.endswith(".json")]
+        return light_files
+    
+
+    def ping(self):
+        """
+        You call ping I print pong.
+        """
+        print("ping-pong")
---
+++ main.py
@@ -0,0 +1,69 @@
+from typing import Union
+import os
+from fastapi import FastAPI
+from data import PN
+import json
+
+app = FastAPI()
+
+barrier_list = PN().get_barrier_data()
+bell_list = PN().get_bell_data()
+light_list = PN().get_light_data()
+barrier_path = os.path.join(PN().root_path,"data","barrier_files")
+bell_path = os.path.join(PN().root_path,"data","bell_files")
+light_path = os.path.join(PN().root_path,"data","light_files")
+
+@app.get("/")
+def read_root():
+    os.path
+    return {"Hello": "World"}
+
+
+#Get barrier without id
+@app.get("/barriers")
+def read_barrier():
+    return {f"barrier n°{k}":json.load(open(os.path.join(barrier_path, v))) for k,v in (dict(enumerate(barrier_list,1))).items()}
+
+#Get barrier with id
+@app.get("/barriers/{item_id}")
+def read_barrier_item(item_id: int):
+    barrier_file_size = len(barrier_list)
+    if item_id > barrier_file_size :
+        return {f"We have no barrier n°{item_id}in  our data source":[]}
+    else :
+        barrier_file = f"{barrier_path}/barrier{item_id}.json"
+        return {f"barrier n°{item_id}": json.load(open(barrier_file))}
+
+
+
+#Get bell without id
+@app.get("/bells")
+def read_bell(q):
+    return {f"bell n°{k}":json.load(open(os.path.join(bell_path, v))) for k,v in (dict(enumerate(bell_list,1))).items()}
+
+#Get bell with id
+@app.get("/bells/{item_id}")
+def read_bell_item(item_id: int):
+    bell_file_size = len(bell_list)
+    if item_id > bell_file_size :
+        return {f"We have no bell n°{item_id} in our data source":[]}
+    else :
+        bell_file = f"{bell_path}/bell{item_id}.json"
+        return {f"bell n°{item_id}": json.load(open(bell_file))}
+
+
+
+#Get light without id
+@app.get("/lights")
+def read_light():
+    return {f"light n°{k}":json.load(open(os.path.join(light_path, v))) for k,v in (dict(enumerate(light_list,1))).items()}
+
+#Get light with id
+@app.get("/lights/{item_id}")
+def read_light_item(item_id: int):
+    light_file_size = len(light_list)
+    if item_id > light_file_size :
+        return {f"We have no light n°{item_id} in our data source":[]}
+    else :
+        light_file = f"{light_path}/light{item_id}.json"
+        return {f"light n°{item_id}": json.load(open(light_file))}
