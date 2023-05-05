3b71cff9bce83c533b7f3acb74fe4a62c3201e00
Lavilley <lavilleyvincent@gmail.com>
2023-05-05 16:50:55 +0200

    MAJ get

--- get_data.py
+++ get_data.py
@@ -10,9 +10,25 @@ url = "http://127.0.0.1:8000/"
 def get_barriers():
     query = url + "barriers"
     response_bar = requests.get(query).json()
-    save_file = open("data/barrier_files/whole_barrier_json", "w")
+    save_file = open("data/barrier_files/all_barriers_json", "w")
     json.dump(response_bar, save_file, indent= 6)
-    print("JSON SAVED")
+    print("barriers JSON file saved !")
+    save_file.close()
+
+def get_bells():
+    query = url + "bells"
+    response_bel = requests.get(query).json()
+    save_file = open("data/bell_files/all_bells_json", "w")
+    json.dump(response_bel, save_file, indent= 6)
+    print("bells JSON file saved !")
+    save_file.close()
+
+def get_lights():
+    query = url + "lights"
+    response_lg = requests.get(query).json()
+    save_file = open("data/light_files/all_lights_json", "w")
+    json.dump(response_lg, save_file, indent= 6)
+    print("lights JSON file saved !")
     save_file.close()
 
 def read_json():
@@ -21,5 +37,4 @@ def read_json():
     my_json_file = json.load(f)
     print(my_json_file)
         
-get_barriers()
-read_json()
+
